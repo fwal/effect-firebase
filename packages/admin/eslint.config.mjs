532d505d589155d8191b1735fbfe6cfed932890e
Frederik Wallner <frederik.wallner@gmail.com>
2025-10-06 08:06:41 +0200

    Linting

--- packages/admin/eslint.config.mjs
+++ packages/admin/eslint.config.mjs
@@ -6,4 +6,19 @@ export default [
     files: ['**/*.ts', '**/*.js'],
     rules: {},
   },
+  {
+    files: ['*.json'],
+    languageOptions: {
+      parser: jsoncEslintParser,
+    },
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}'],
+          ignoredDependencies: ['@effect-firebase/core'],
+        },
+      ],
+    },
+  },
 ];
