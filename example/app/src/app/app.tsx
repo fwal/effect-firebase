d1270df1c67e3e2bb51dae7f7ad789ac2d937c44
Frederik Wallner <frederik.wallner@gmail.com>
2025-11-04 20:33:59 +0100

    Add example request

--- example/app/src/app/app.tsx
+++ example/app/src/app/app.tsx
@@ -1,9 +1,29 @@
-import NxWelcome from './nx-welcome';
+import { initializeApp } from 'firebase/app';
+import {
+  getFunctions,
+  connectFunctionsEmulator,
+  httpsCallable,
+} from 'firebase/functions';
+import SendRequest from './send-request';
 
 export function App() {
+  const app = initializeApp({
+    projectId: 'effect-firebase-example',
+  });
+  const functions = getFunctions(app, 'europe-north1');
+  connectFunctionsEmulator(functions, 'localhost', 5001);
+
   return (
     <div>
-      <NxWelcome title="app" />
+      <SendRequest
+        title="onExampleCall"
+        description="Call the onExampleCall function"
+        showInput
+        inputPlaceholder='{"id": 123}'
+        onSendRequest={(input) =>
+          httpsCallable(functions, 'onExampleCall')(input)
+        }
+      />
     </div>
   );
 }
