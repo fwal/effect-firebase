0309c57ad0dd48f2e87ff4ad58e0296788191532
Frederik Wallner <frederik.wallner@gmail.com>
2025-11-06 07:41:40 +0100

    Update date tests

--- tests/src/lib/firestore/schema/date.spec.ts
+++ tests/src/lib/firestore/schema/date.spec.ts
@@ -1,31 +1,41 @@
 import { Effect, Schema } from 'effect';
 import { describe, it, expect } from '@effect/vitest';
-import { DateFromFirestore, UnexpectedTypeError } from 'effect-firebase';
-import { firestoreService } from '@effect-firebase/mock';
+import { FirestoreSchema, UnexpectedTypeError } from 'effect-firebase';
+import { MockFirestoreService, MockTimestamp } from '@effect-firebase/mock';
 
-describe('DateFromFirestore', () => {
+describe('FirestoreSchema.Date', () => {
   it.effect('Calls service to convert to timestamp when encoding', () =>
     Effect.gen(function* () {
       const date = new Date();
-      const convertToTimestamp = vi.fn(() => Effect.succeed('from callback'));
-      const service = firestoreService({ convertToTimestamp });
-      const result = yield* Schema.encodeUnknown(DateFromFirestore)(date).pipe(
-        Effect.provide(service)
-      );
+      const timestamp = MockTimestamp.fromDate(date);
+
+      const convertToTimestamp = vi.fn(() => Effect.succeed(timestamp));
+      const service = MockFirestoreService({ convertToTimestamp });
+
+      const result = yield* Schema.encodeUnknown(FirestoreSchema.Date)(
+        date
+      ).pipe(Effect.provide(service));
+
       expect(convertToTimestamp).toHaveBeenCalledWith(date);
-      expect(result).toEqual('from callback');
+      expect(result).toEqual(timestamp);
     })
   );
 
   it.effect('Calls service to convert from timestamp when decoding', () =>
     Effect.gen(function* () {
       const date = new Date();
-      const convertFromTimestamp = vi.fn(() => Effect.succeed(date));
-      const service = firestoreService({ convertFromTimestamp });
-      const result = yield* Schema.decodeUnknown(DateFromFirestore)(
-        'MockTimestamp'
+      const timestamp = MockTimestamp.fromDate(date);
+
+      const convertFromTimestamp = vi.fn((timestamp) =>
+        Effect.succeed(timestamp.toDate())
+      );
+      const service = MockFirestoreService({ convertFromTimestamp });
+
+      const result = yield* Schema.decodeUnknown(FirestoreSchema.Date)(
+        timestamp
       ).pipe(Effect.provide(service));
-      expect(convertFromTimestamp).toHaveBeenCalledWith('MockTimestamp');
+
+      expect(convertFromTimestamp).toHaveBeenCalledWith(timestamp);
       expect(result).toEqual(date);
     })
   );
@@ -40,10 +50,47 @@ describe('DateFromFirestore', () => {
           })
         )
       );
-      const service = firestoreService({ convertFromTimestamp });
-      yield* Schema.decodeUnknown(DateFromFirestore)('MockTimestamp').pipe(
+      const service = MockFirestoreService({ convertFromTimestamp });
+
+      yield* Schema.decodeUnknown(FirestoreSchema.Date)('NotATimestamp').pipe(
         Effect.provide(service)
       );
+    })
+  );
+
+  it.effect('Decodes timestamp to date', () =>
+    Effect.gen(function* () {
+      const date = new Date();
+      const timestamp = MockTimestamp.fromDate(date);
+
+      const service = MockFirestoreService();
+
+      const schema = Schema.Struct({
+        createdAt: FirestoreSchema.Date,
+      });
+
+      const result = yield* Schema.decodeUnknown(schema)({
+        createdAt: timestamp,
+      }).pipe(Effect.provide(service));
+      expect(result).toEqual({ createdAt: date });
+    })
+  );
+
+  it.effect('Encodes date to timestamp', () =>
+    Effect.gen(function* () {
+      const date = new Date();
+      const timestamp = MockTimestamp.fromDate(date);
+
+      const service = MockFirestoreService();
+
+      const schema = Schema.Struct({
+        createdAt: FirestoreSchema.Date,
+      });
+
+      const result = yield* Schema.encodeUnknown(schema)({
+        createdAt: date,
+      }).pipe(Effect.provide(service));
+      expect(result).toEqual({ createdAt: timestamp });
     })
   );
 });
