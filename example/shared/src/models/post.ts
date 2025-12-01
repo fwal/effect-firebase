656d16cd5414a7eebcf7936e813099ce0a8d2fe2
Frederik Wallner <frederik.wallner@gmail.com>
2025-12-01 13:53:44 +0100

    chore: smaller variable name

--- example/shared/src/models/post.ts
+++ example/shared/src/models/post.ts
@@ -1,14 +1,14 @@
 import { Schema } from 'effect';
-import { Model as SM } from '@effect/sql';
-import { Model as FM } from 'effect-firebase';
+import { Model as M } from '@effect/sql';
+import { Model as F } from 'effect-firebase';
 
 export const PostId = Schema.String.pipe(Schema.brand('PostId'));
 export const postId = Schema.decode(PostId);
 
-export class PostModel extends SM.Class<PostModel>('PostModel')({
-  id: SM.Generated(PostId),
-  createdAt: FM.DateTimeInsert,
-  updatedAt: FM.DateTimeUpdate,
+export class PostModel extends M.Class<PostModel>('PostModel')({
+  id: M.Generated(PostId),
+  createdAt: F.DateTimeInsert,
+  updatedAt: F.DateTimeUpdate,
   title: Schema.String,
   content: Schema.String,
 }) {}
