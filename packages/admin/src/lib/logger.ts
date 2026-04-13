cc726f9f705274ed9dfeef7c96c337b5bc240e5e
Frederik Wallner <frederik.wallner@gmail.com>
2026-04-13 21:22:45 +0200

    chore: migrate logger

--- packages/admin/src/lib/logger.ts
+++ packages/admin/src/lib/logger.ts
@@ -2,37 +2,33 @@ import { Logger, LogLevel, Match } from 'effect';
 import { logger } from 'firebase-functions';
 
 type LoggerFunction = typeof logger.debug;
-type LogLevelLabel = LogLevel.LogLevel['label'];
 
 const functionForLogLevel: (
-  logLevel: LogLevelLabel
+  logLevel: LogLevel.LogLevel
 ) => LoggerFunction | null = (value) =>
   Match.value(value).pipe(
-    Match.when('DEBUG', () => logger.debug),
-    Match.when('TRACE', () => logger.debug),
-    Match.when('INFO', () => logger.info),
-    Match.when('WARN', () => logger.warn),
-    Match.when('ERROR', () => logger.error),
-    Match.when('FATAL', () => logger.error),
-    Match.when('ALL', () => null),
-    Match.when('OFF', () => null),
+    Match.when('Debug', () => logger.debug),
+    Match.when('Trace', () => logger.debug),
+    Match.when('Info', () => logger.info),
+    Match.when('Warn', () => logger.warn),
+    Match.when('Error', () => logger.error),
+    Match.when('Fatal', () => logger.error),
+    Match.when('All', () => null),
+    Match.when('None', () => null),
     Match.exhaustive
   );
 
 /**
  * A logger that writes to the Firebase Functions console.
  */
-const cloudConsoleLogger = Logger.map(
-  Logger.structuredLogger,
-  ({ logLevel, message, annotations, spans, fiberId, cause }) => {
-    const func = functionForLogLevel(logLevel as LogLevelLabel);
-    if (!func) {
-      return;
-    }
-    const messageArray = Array.isArray(message) ? message : [message];
-    return func(...messageArray, { annotations, spans, fiberId, cause });
+const cloudConsoleLogger = Logger.make(({ logLevel, message }) => {
+  const func = functionForLogLevel(logLevel);
+  if (!func) {
+    return;
   }
-);
+  const messageArray = Array.isArray(message) ? message : [message];
+  return func(...messageArray);
+});
 
 /**
  * A logger that writes to the Firebase Functions console.
@@ -45,7 +41,4 @@ const cloudConsoleLogger = Logger.map(
  * Effect.log('Hello, world!').pipe(Effect.provide(Logger.cloudConsole));
  * ```
  */
-export const cloudConsole = Logger.replace(
-  Logger.defaultLogger,
-  cloudConsoleLogger
-);
+export const cloudConsole = Logger.layer([cloudConsoleLogger]);
